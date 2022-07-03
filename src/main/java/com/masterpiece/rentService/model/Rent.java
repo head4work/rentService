eebd4e4e4fbe4ca3e8fd581c69f5cf0865cb23a6
Igor <head4work@gmail.com>
2022-07-03 14:13:11 -0700

    User constructor added , Rent obj added

--- src/main/java/com/masterpiece/rentService/model/Rent.java
+++ src/main/java/com/masterpiece/rentService/model/Rent.java
@@ -1,7 +1,16 @@
 package com.masterpiece.rentService.model;
 
+import java.util.Set;
+
 public class Rent extends AbstractNamedEntity {
 
+    private String description;
+
+    private String image;
+
     private Boolean available;
+
+    private Set<Status> statuses;
+
 
 }

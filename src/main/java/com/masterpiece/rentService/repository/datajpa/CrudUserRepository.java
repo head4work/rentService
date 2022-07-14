d33cfcc79c0980e38dc1db664718de390dacdb1f
Igor <head4work@gmail.com>
2022-07-14 15:23:03 -0700

    database initialization scripts added

--- src/main/java/com/masterpiece/rentService/repository/datajpa/CrudUserRepository.java
+++ src/main/java/com/masterpiece/rentService/repository/datajpa/CrudUserRepository.java
@@ -1,10 +1,27 @@
 package com.masterpiece.rentService.repository.datajpa;
 
 import com.masterpiece.rentService.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-@Transactional(readOnly = true)
-public interface CrudUserRepository extends JpaRepository<User,Integer> {
+import javax.persistence.QueryHint;
 
+@Transactional(readOnly = true)
+public interface CrudUserRepository extends JpaRepository<User, Integer> {
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM User u WHERE u.id=:id")
+    int delete(@Param("id") int id);
+
+    //  https://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/Hibernate_User_Guide.html#hql-distinct
+    @QueryHints({
+            @QueryHint(name = org.hibernate.jpa.QueryHints.HINT_PASS_DISTINCT_THROUGH, value = "false")
+    })
+    User getByEmail(String email);
+
+    //    https://stackoverflow.com/a/46013654/548473
+    @EntityGraph(attributePaths = {"meals"}, type = EntityGraph.EntityGraphType.LOAD)
+    @Query("SELECT u FROM User u WHERE u.id=?1")
+    User getWithMeals(int id);
 }
--- src/main/java/com/masterpiece/rentService/repository/datajpa/DataJpaUserRepository.java
+++ src/main/java/com/masterpiece/rentService/repository/datajpa/DataJpaUserRepository.java
@@ -2,44 +2,48 @@ package com.masterpiece.rentService.repository.datajpa;
 
 import com.masterpiece.rentService.model.User;
 import com.masterpiece.rentService.repository.UserRepository;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public class DataJpaUserRepository implements UserRepository {
+    private static final Sort SORT_NAME_EMAIL = Sort.by(Sort.Direction.ASC, "name", "email");
 
-    private final CrudUserRepository crudUserRepository;
+    private final CrudUserRepository crudRepository;
 
-    public DataJpaUserRepository(CrudUserRepository crudUserRepository) {
-        this.crudUserRepository = crudUserRepository;
+    public DataJpaUserRepository(CrudUserRepository crudRepository) {
+        this.crudRepository = crudRepository;
     }
 
     @Override
-    @Transactional
     public User save(User user) {
-        crudUserRepository.save(user);
-        return null;
+        return crudRepository.save(user);
     }
 
     @Override
     public boolean delete(int id) {
-        return false;
+        return crudRepository.delete(id) != 0;
     }
 
     @Override
     public User get(int id) {
-        return null;
+        return crudRepository.findById(id).orElse(null);
     }
 
     @Override
     public User getByEmail(String email) {
-        return null;
+        return crudRepository.getByEmail(email);
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        return crudRepository.findAll(SORT_NAME_EMAIL);
     }
+
+/*    @Override
+    public User getWithMeals(int id) {
+        return crudRepository.getWithMeals(id);
+    }*/
 }

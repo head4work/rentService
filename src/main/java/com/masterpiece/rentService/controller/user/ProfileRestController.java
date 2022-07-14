024194780f07935a7d4d5d5557ffa32445bb503d
Igor <head4work@gmail.com>
2022-07-14 12:32:40 -0700

    Users code transfer from topjava

---
+++ src/main/java/com/masterpiece/rentService/controller/user/ProfileRestController.java
@@ -0,0 +1,36 @@
+package com.masterpiece.rentService.controller.user;
+
+import com.masterpiece.rentService.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import static com.masterpiece.rentService.util.SecurityUtil.authUserId;
+
+@RestController
+@RequestMapping(value = ProfileRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+public class ProfileRestController extends AbstractUserController {
+    static final String REST_URL = "/rest/profile";
+
+    @GetMapping
+    public User get() {
+        return super.get(authUserId());
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete() {
+        super.delete(authUserId());
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@RequestBody User user) {
+        super.update(user, authUserId());
+    }
+
+    @GetMapping("/text")
+    public String testUTF() {
+        return "Русский текст";
+    }
+}
---
+++ src/main/java/com/masterpiece/rentService/util/SecurityUtil.java
@@ -0,0 +1,20 @@
+package com.masterpiece.rentService.util;
+
+import com.masterpiece.rentService.model.AbstractBaseEntity;
+
+public class SecurityUtil {
+
+    private static int id = AbstractBaseEntity.START_SEQ;
+
+    private SecurityUtil() {
+    }
+
+    public static int authUserId() {
+        return id;
+    }
+
+    public static void setAuthUserId(int id) {
+        SecurityUtil.id = id;
+    }
+
+}

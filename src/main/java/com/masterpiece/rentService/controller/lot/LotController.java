78b32e0e2e2e13ec0f036f21361e20bd3b3c0f0d
Igor <head4work@gmail.com>
2022-08-03 15:12:46 -0700

    LotController started

--- src/main/java/com/masterpiece/rentService/controller/lot/LotController.java
+++ src/main/java/com/masterpiece/rentService/controller/lot/LotController.java
@@ -1,12 +1,26 @@
 package com.masterpiece.rentService.controller.lot;
 
 import com.masterpiece.rentService.service.LotService;
-import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/lots")
 public class LotController {
+
 
     private LotService lotService;
 
 
+    public LotController(LotService lotService) {
+        this.lotService = lotService;
+    }
+
+    @GetMapping
+    public String listLots(Model model) {
+        model.addAttribute("lots", lotService.getAll());
+        return "lots";
+    }
 }
--- src/main/java/com/masterpiece/rentService/util/LotUtil.java
+++ src/main/java/com/masterpiece/rentService/util/LotUtil.java
@@ -1,15 +1,24 @@
 package com.masterpiece.rentService.util;
 
 import com.masterpiece.rentService.model.Lot;
+import com.masterpiece.rentService.model.Rent;
 import com.masterpiece.rentService.model.to.LotTo;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class LotUtil {
     public static List<Lot> getALlWithDatesReduced(List<Lot> lots) {
         lots.forEach(lot -> lot.getRents().removeIf(rent -> rent.getEndRentDate().compareTo(LocalDateTime.now()) < 0));
         return lots;
+    }
+
+    public static Rent saveRent(LocalDateTime start, LocalDateTime end, int lotId, int userId) {
+        Map<LocalDate, Integer> map = new ConcurrentHashMap<>();
+        return null;
     }
 
     public static List<LotTo> lotTos(List<Lot> lots) {

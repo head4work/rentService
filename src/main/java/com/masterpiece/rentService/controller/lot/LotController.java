8ed0e272ad98a2b2a95a5412f776e164b588e131
igor <head4work@gmail.com>
2022-08-04 17:25:48 -0700

    LotController delete done

--- src/main/java/com/masterpiece/rentService/controller/lot/LotController.java
+++ src/main/java/com/masterpiece/rentService/controller/lot/LotController.java
@@ -4,6 +4,7 @@ import com.masterpiece.rentService.service.LotService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,5 +23,11 @@ public class LotController {
     public String listLots(Model model) {
         model.addAttribute("lots", lotService.getAll());
         return "lots";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteLot(@PathVariable int id) {
+        lotService.delete(id);
+        return "redirect:/lots";
     }
 }

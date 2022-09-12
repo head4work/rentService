a9833f910d236b084c727a9ffe7ceb13afc5d2af
Igor <head4work@gmail.com>
2022-09-12 13:19:42 -0700

    JavaScript datePicker added

---
+++ src/main/java/com/masterpiece/rentService/controller/lot/LotRestController.java
@@ -0,0 +1,14 @@
+package com.masterpiece.rentService.controller.lot;
+
+import com.masterpiece.rentService.service.LotService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+
+public class LotRestController {
+    private LotService lotService;
+
+
+}
